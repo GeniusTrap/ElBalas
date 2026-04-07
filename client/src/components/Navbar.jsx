07ae5d6c3ebd71f2cf617b86d5678f3f96ce32d0
GENIUSTRAP <geniustrap69@gmail.com>
2026-04-08 00:38:28 +0200

    Fixing href URL

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = () => {
         Contact
       </button>
       <a 
-        href="http://localhost:5174" 
+        href="https://elbalas-admin.vercel.app" 
         target="_blank"
         rel="noopener noreferrer"
         className="bg-gradient-to-r from-yellow-600 to-yellow-500 text-black px-4 py-2 rounded-md font-semibold text-center mx-4 mb-2"
